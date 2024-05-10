ea433c00b5a4d147a67cf71e8fe34785856758cc
Sebastián Rave <jsravem@eafit.edu.co>
2024-05-09 23:39:27 -0500

    Assignment develop branch

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        assignment3();
+    }
+
+    static void assignment3(){
+        System.out.println("Assignment 3 by Juan Sebastián Rave Martínez");
+    }
+}
